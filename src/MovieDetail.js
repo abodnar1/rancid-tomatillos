f3bea6922834c4029a252c3b3cef9eae5c70c4b4
Grant Beard <Grantxbeard@gmail.com>
2022-07-09 16:52:35 -0600

    add stylesheet for movie detail

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import "./MovieDetail.css"
 
 const MovieDetail = ({ details }) => {
   return (
-    <div>
-      <div>
+    <div className="detail-card">
         <h2>{details.title}</h2>
-        <img src={details.backdrop_path}/>
-      </div>
-      <div>
-        <div>
+        <img className="backdrop" src={details.backdrop_path}/>
+      <div className="copy-wrapper">
+        <div className="copy">
+          <p>Tagline</p>
+          <p>Overview</p>
+          <p>Genres</p>
+          <p>Runtime</p>
         </div>
-        <div>
+        <div className="copy">
+        <p>Budget/Revenue</p>
+        <p>{details.average_rating}</p>
+        <p>{details.release_date}</p>
         </div>
       </div>
     </div>
