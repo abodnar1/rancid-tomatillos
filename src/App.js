fcca356f9d0053fb93c884c705a3ec846fd33e3e
Grant Beard <Grantxbeard@gmail.com>
2022-07-10 15:43:23 -0600

    add error handling to fetch

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
       movies: [],
       clickedMovie: null,
       isLoading: false,
+      error: '',
     }
   }
 
@@ -18,12 +19,22 @@ class App extends Component {
     this.setState({isLoading: true})
 
     fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
-      .then(res => res.json())
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          console.log("Error")
+        }
+      })
       .then(data => {
         this.setState({
           movies: data.movies,
           isLoading: false,
         })
+      })
+      .catch(error => {
+        this.setState({error: error.message})
+        console.log("error")
       })
   }
 
@@ -44,6 +55,7 @@ class App extends Component {
           <h1>Rancid Tomatillos</h1>
         </header>
           {this.state.isLoading && <p>Loading...</p>}
+          {this.state.error && <h2>{this.state.error}</h2>}
           {this.state.clickedMovie ? 
           <MovieDetail details={this.state.clickedMovie} closeMovieDetails={this.closeMovieDetails} /> : 
           <Movies movies={this.state.movies} displayMovieDetails={this.displayMovieDetails} /> }
