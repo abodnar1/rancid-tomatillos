9d896f235f7595b8950ae4ffa93c675542b957b4
Amber Bodnar <amber.bodnar@gmail.com>
2022-07-14 22:28:21 -0400

    Add test for MovieDetail exit button

--- cypress/e2e/MovieDetail_spec.cy.js
+++ cypress/e2e/MovieDetail_spec.cy.js
@@ -1,24 +1,22 @@
-describe("Rancid Tomatillos", () => {
+describe('Rancid Tomatillos', () => {
 
   beforeEach(() => {
-    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", { fixture: "movieDetail" })
-    cy.visit('http://localhost:3000/')
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/694919', { fixture: 'movieDetail' })
     cy.visit('http://localhost:3000/694919')
   });
 
   it('As a user, I should see the expanded details for a specific movie', () => {
-    cy.get("h2").contains("Money Plane")
-    cy.get("p").last().contains("2020-09-29")
-    cy.get(".runtime").contains("82 min.")
+    cy.get('h2').contains('Money Plane')
+    cy.get('p').last().contains('2020-09-29')
+    cy.get('.runtime').contains('82 min.')
   });
 
+  it('As a user, I should be able to click the X button of the movie details page to return to home', () => {
+    cy.get('.material-symbols-outlined').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  });
 
 });
 
-//API CALLS (testing)
-// 1)
-// A) As a user, I should be able to navigate back to the home page and to the details through URL. (happy)
-
-
-//2)
-// A) As a user, I should be able to click the X button of the movie details page to return to home. (happy)
+// Additional tests needed?
+// As a user, I should be able to navigate back to the home page and to the details through URL
--- cypress/e2e/movies_spec.cy.js
+++ cypress/e2e/movies_spec.cy.js
@@ -1,24 +1,24 @@
-describe("Rancid Tomatillos", () => {
+describe('Rancid Tomatillos', () => {
 
   beforeEach(() => {
-    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", { fixture: "movies" })
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { fixture: 'movies' })
     cy.visit('http://localhost:3000/')
   });
 
   it('The user should be able to visit http://localhost:3000 and see a title', () => {
-    cy.get(".header").contains("Rancid Tomatillos").should("be.visible")
+    cy.get('.header').contains('Rancid Tomatillos').should('be.visible')
   });
 
   it('The user should be able to visit http://localhost:3000 and see all movies', () => {
-    cy.get("section").should("have.class", "movie-container")
-    cy.get(".movie-poster").should("have.length", 40)
+    cy.get('section').should('have.class', 'movie-container')
+    cy.get('.movie-poster').should('have.length', 40)
   });
 
-  it("The should be able to click on a movie card and be directed to a new URL", () => {
-    cy.get(".movie-poster").first().click()
+  it('The should be able to click on a movie card and be directed to a new URL', () => {
+    cy.get('.movie-poster').first().click()
     cy.url().should('eq', 'http://localhost:3000/694919')
   });
-  
+
 });
 
 // Notes from Artan:
@@ -30,7 +30,7 @@ describe("Rancid Tomatillos", () => {
 // cy.get.first
 // cy.get.click (for buttons)
 // cy.url().should('eq', 'http://localhost:3000/')
-// cy.get.type("string of what user is typing in")
+// cy.get.type('string of what user is typing in')
 // cy.get.array.last
 // cy.get.at(indexofarray)
 // cy.get('.category__header').eq(1).contains('Best Director') (eq is checking index 1)
